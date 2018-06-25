1ac8b3e7894c207cc01f8da09c1def2c6f66865c
Zodiac <zodiac182@outlook.com>
2018-06-25 17:21:01 +0800

    Update smzdm.py
    update

--- smzdm.py
+++ smzdm.py
@@ -11,7 +11,11 @@ import zlib
 import time
 import getpass
 
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.INFO,
+                                format='%(asctime)s %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='smzdm.log',
+                filemode='w+')
 reload(sys)
 sys.setdefaultencoding("utf8")
 
@@ -19,99 +23,96 @@ true = 1
 false = 0
 
 class SMZDM(object):
- 
-	def __init__(self,name,is_sigin):
-		self.name = name;
-		self.password = '';
-		self.is_signin = is_sigin;
-		self.sessionObj = requests.Session()
-	 
-	def _getHeaders(self):
-		headers = {
-			'Accept':'application/json, text/javascript, */*; q=0.01',
-			'Accept-Encoding':'gzip, deflate, br',
-			'Accept-Language':'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4',
-			'Connection':'keep-alive',
-			'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-			'Host':'zhiyou.smzdm.com',
-			'Origin':'https://zhiyou.smzdm.com',
-			'Referer':'https://zhiyou.smzdm.com/user/login',
-			'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-			'X-Requested-With':'XMLHttpRequest'
-		} 
-		return headers
 
-	def _getSigninHeaders(self):
-		headers = {
-			'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
-		}
-		return headers
-	 
-	def login(self):
-		'''登录网站'''
-		logging.debug(u'正在登陆 username : %s password : %s' %(self.name,self.password))
-		logging.debug(u'\nheaders is : %s' % self._getHeaders())
-		loginparams = {'username': self.name,'password':self.password,'remember':'0','captcha':'0'}
-		logging.debug(u'\nloginparams is : %s' % loginparams)
-		req = self.sessionObj.post(r'https://zhiyou.smzdm.com/user/login/ajax_check', data=loginparams,headers=self._getHeaders())
+        def __init__(self,name,password,is_sigin):
+                self.name = name;
+                self.password = password;
+                self.is_signin = is_sigin;
+                self.sessionObj = requests.Session()
 
+        def _getHeaders(self):
+                headers = {
+                        'Accept':'application/json, text/javascript, */*; q=0.01',
+                        'Accept-Encoding':'gzip, deflate, br',
+                        'Accept-Language':'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4',
+                        'Connection':'keep-alive',
+                        'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+                        'Host':'zhiyou.smzdm.com',
+                        'Origin':'https://zhiyou.smzdm.com',
+                        'Referer':'https://zhiyou.smzdm.com/user/login',
+                        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+                        'X-Requested-With':'XMLHttpRequest'
+                }
+                return headers
 
-		print req.text
+        def _getSigninHeaders(self):
+                headers = {
+                        'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+                }
+                return headers
 
-		params = eval(req.text)
+        def login(self):
+                '''登录网站'''
+                logging.debug(u'正在登陆 username : %s password : %s' %(self.name,self.password))
+                logging.debug(u'\nheaders is : %s' % self._getHeaders())
+                loginparams = {'username': self.name,'password':self.password,'remember':'0','captcha':'0'}
+                logging.debug(u'\nloginparams is : %s' % loginparams)
+                req = self.sessionObj.post(r'https://zhiyou.smzdm.com/user/login/ajax_check', data=loginparams,headers=self._getHeaders())
 
-		if(params['error_code'] == 0):
-			print str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time() )) ) +' '+self.name+u'登陆成功.\n'
-			return true
-		else:
-			print str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time() )) )+' '+self.name+u'登录失败.' + params['error_msg'].decode('unicode_escape')
-			return false
+                params = eval(req.text)
 
-		return false
-			 
-	def sign(self):
-		headers = {
-			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0',
-			'Host': 'zhiyou.smzdm.com',
-			'Referer': 'http://www.smzdm.com/'
-		}
-		req = self.sessionObj.get(r'http://www.smzdm.com/user/checkin/jsonp_checkin', headers=headers)
-		try:
-			params = eval(req.text)
-			if(params['error_code'] == 0):
-				print str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time() )) )+' '+self.name+u'签到成功.\n'
-				return true;
-			else:
-				print str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time() )) ) +' '+self.name+u'签到失败.' + params['error_msg'].decode('unicode_escape')
-				return false;
-		except Exception as e:
-			print str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time() )) )+' '+self.name+u'异常！签到失败: ',e
-			return false;
+                if(params['error_code'] == 0):
+                        logging.info(self.name+u'登陆成功.\n')
+                        return true
+                else:
+                        logging.warning(str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time() )) )+' '+self.name+u'登录失败.' + params['error_msg'].decode('unicode_escape'))
+"smzdm.py" 115L, 3663C                                                                                                                                                                    1,1          顶端
+                else:
+                        logging.warning(str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time() )) )+' '+self.name+u'登录失败.' + params['error_msg'].decode('unicode_escape'))
+                        return false
+
+                return false
+
+        def sign(self):
+                headers = {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0',
+                        'Host': 'zhiyou.smzdm.com',
+                        'Referer': 'http://www.smzdm.com/'
+                }
+                req = self.sessionObj.get(r'http://www.smzdm.com/user/checkin/jsonp_checkin', headers=headers)
+                try:
+                        params = eval(req.text)
+                        if(params['error_code'] == 0):
+                                logging.info(self.name+u'签到成功.\n')
+                                return true;
+                        else:
+                                logging.warning(self.name+u'签到失败.' + params['error_msg'].decode('unicode_escape'))
+                                return false;
+                except Exception as e:
+                        logging.ERROR(self.name+u'异常！签到失败: '+e)
+                        return false;
 
 if __name__ == '__main__':
-	last_signin_date = '0000-00-00'
-	userlogin = [SMZDM('uname',false)]
-	
-	'''输入密码'''
-	for usr in userlogin:
-		tmp_pwd = getpass.getpass('Input the password for %s:' %(usr.name) )
-		usr.password = tmp_pwd
-	while(True):
-		current_time = time.localtime(time.time())
-		if(current_time.tm_hour == 0):
-			print(str( time.strftime('%Y-%m-%d %H:%M:%S',current_time) )+u' 新的一天开始了.\n');
-			
-		for usr in userlogin:
-			if(time.strftime('%Y-%m-%d',current_time) == last_signin_date):
-				usr.is_signin = true;
-			else:
-				usr.is_signin = false;
-		
-			if(usr.is_signin == false):
-				if(usr.login() == true):
-					if(usr.sign() == true):
-						usr.is_signin = true;
-				
-		last_signin_date = time.strftime('%Y-%m-%d',current_time);				
-		
-		time.sleep(3600);
+        last_signin_date = '0000-00-00'
+        userlogin = [SMZDM('usr1','pwd1',false),SMZDM('usr2','pwd2',false),SMZDM('usr3','pwd3',false)]
+
+        while(True):
+                current_time = time.localtime(time.time())
+                if(time.strftime('%Y-%m-%d',current_time) != last_signin_date):
+                        logging.debug(u' 新的一天开始了.\n');
+                        for usr in userlogin:
+                                usr.is_signin = false
+
+                for usr in userlogin:
+                        try:
+                                if(usr.is_signin == false):
+                                        if(usr.login() == true):
+                                                if(usr.sign() == true):
+                                                        usr.is_signin = true;
+                        except Exception,e:
+                                last_signin_date = '0000-00-00'
+                                logging.error(e.message)
+
+                last_signin_date = time.strftime('%Y-%m-%d',current_time);
+
+                time.sleep(3600);
